0b405ec664d5494ec1b362bb0246e97ab1401f36
4LL3N51147 <s54wp868wLPs>
2021-09-27 11:22:10 +0800

    Project Setup

---
+++ src/main/java/com/allengcc/wowauctionassistant/WowAuctionAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.allengcc.wowauctionassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowAuctionAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowAuctionAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allengcc/wowauctionassistant/WowAuctionAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allengcc.wowauctionassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowAuctionAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
